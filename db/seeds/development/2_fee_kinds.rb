624b3863ddf94821c8e65c3c4d58b85d6a3cca5c
Carlo Beltrame <beltrame@puzzle.ch>
2026-06-02 18:10:58 +0200

    Fix seeding on deployments (hitobito/hitobito_dpsg#18)

--- db/seeds/development/2_fee_kinds.rb
+++ db/seeds/development/2_fee_kinds.rb
@@ -1,3 +1,10 @@
-require Rails.root.join("..", "hitobito_pfadi_de", "db", "seeds", "support", "fee_kinds_seeder")
+# frozen_string_literal: true
+
+#  Copyright (c) 2012-2026, Bund der Pfadfinderinnen und Pfadfinder e.V.. This file is part of
+#  hitobito_bdp and licensed under the Affero General Public License version 3
+#  or later. See the COPYING file at the top-level directory or at
+#  https://github.com/hitobito/hitobito_bdp.
+
+require HitobitoPfadiDe::Wagon.root.join("db", "seeds", "support", "fee_kinds_seeder")
 
 FeeKindsSeeder.new.seed_fee_kinds
